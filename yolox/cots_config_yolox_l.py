fcecfa09ef0f7722b7f257f0a51c3220e3376adc
masa-ymd <masa-ymd@nifty.com>
2022-01-09 18:52:27 +0000

    x

---
+++ yolox/cots_config_yolox_l.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+from yolox.exp import Exp as MyExp
+
+TRAIN_PATH = '/root/kaggle/tensorflow-great-barrier-reef/data'
+COCO_DATASET_PATH = f'{TRAIN_PATH}/cocodataset'
+OUTPUT_DIR = '/root/kaggle/tensorflow-great-barrier-reef/models'
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.depth = 1.0
+        self.width = 1.0
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
+        
+        # Define yourself dataset path
+        self.data_dir = f"{COCO_DATASET_PATH}"
+        self.train_ann = "train.json"
+        self.val_ann = "valid.json"
+
+        self.output_dir = f'{OUTPUT_DIR}//YOLOX_outputs'
+
+        self.num_classes = 1
+
+        self.max_epoch = 30
+        self.data_num_workers = 2
+        self.eval_interval = 1
+        
+        self.mosaic_prob = 1.0
+        self.mixup_prob = 1.0
+        self.hsv_prob = 1.0
+        self.flip_prob = 0.5
+        self.no_aug_epochs = 2
+        
+        self.input_size = (960, 960)
+        self.mosaic_scale = (0.5, 1.5)
+        self.random_size = (10, 20)
+        self.test_size = (960, 960)
