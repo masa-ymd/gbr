909e7627875e87a5d696a33da1e6fdc2d5ed88fc
masa-ymd <masa-ymd@nifty.com>
2022-01-13 16:50:03 +0000

    x

--- coco_dataset_generator.py
+++ coco_dataset_generator.py
@@ -1,6 +1,6 @@
 from ast import literal_eval
 import pandas as pd
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 
 TRAIN_PATH = '/root/kaggle/tensorflow-great-barrier-reef/data/train_images'
 N_SAMP = 6000
@@ -19,3 +19,37 @@ df['path'] = df.apply(lambda row: f"{TRAIN_PATH}/video_{row['video_id']}/{row['v
 
 with pd.option_context("display.max_columns", 100):
     print(df.tail())
+
+def coco(df):
+    
+    annotion_id = 0
+    images = []
+    annotations = []
+
+    categories = [{'id': 0, 'name': 'cots'}]
+
+    for i, row in tqdm(df.iterrows(), total = len(df)):
+
+        images.append({
+            "id": i,
+            "file_name": f"{row['image_id']}.jpg",
+            "height": 720,
+            "width": 1280,
+        })
+        for bbox in row['annotations']:
+            annotations.append({
+                "id": annotion_id,
+                "image_id": i,
+                "category_id": 0,
+                "bbox": list(bbox.values()),
+                "area": bbox['width'] * bbox['height'],
+                "segmentation": [],
+                "iscrowd": 0
+            })
+            annotion_id += 1
+
+    json_file = {'categories':categories, 'images':images, 'annotations':annotations}
+    return json_file
+
+json_train = coco(df[~df['is_valid']])
+json_valid = coco(df[ df['is_valid']])
