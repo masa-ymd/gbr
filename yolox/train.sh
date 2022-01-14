4d50d2365df7a51f18a05e7f7ac1b9565d940ad3
masa-ymd <masa-ymd@nifty.com>
2022-01-14 17:54:46 +0000

    x

--- yolox/train.sh
+++ yolox/train.sh
@@ -3,7 +3,7 @@
 python3 -B train.py \
 -f /root/gbr/yolox/cots_config_yolox_l.py \
 -d 1 \
--b 8 \
+-b 4 \
 --fp16 \
 -o \
 -c /root/kaggle/tensorflow-great-barrier-reef/models/pretrained_weights/yolox_l.pth
