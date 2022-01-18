99b04adc2b67ca0c742885291d773bd1b56c9d6f
masa-ymd <masa-ymd@nifty.com>
2022-01-18 16:22:49 +0000

    x

--- yolov5/train.sh
+++ yolov5/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /root/kaggle/yolov5
-python3 train.py --img 3600 --batch 4 --epochs 15 --data /root/gbr/yolov5/reef_f1_naive.yaml --weights yolov5s6.pt --name l6_3600_uflip_vm5_f1 --hyp /root/gbr/yolov5/hyp.heavy.2.yaml
+python3 train.py --img 3600 --batch 2 --epochs 15 --data /root/gbr/yolov5/reef_f1_naive.yaml --weights yolov5s6.pt --name l6_3600_uflip_vm5_f1 --hyp /root/gbr/yolov5/hyp.heavy.2.yaml
