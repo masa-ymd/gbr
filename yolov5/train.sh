9a058aba19ab15dd86f5a621537a2f8ab9ee9d0a
masa-ymd <masa-ymd@nifty.com>
2022-01-26 18:38:35 +0000

    x

--- yolov5/train.sh
+++ yolov5/train.sh
@@ -3,4 +3,4 @@
 cd /root/kaggle/yolov5
 #python3 train.py --img 4000 --batch 2 --epochs 15 --data /root/gbr/yolov5/reef_f1_naive.yaml --weights yolov5s6.pt --name l6_3600_uflip_vm5_f1 --hyp /root/gbr/yolov5/hyp.heavy.2.yaml
 #python3 train.py --img 3000 --batch 2 --epochs 15 --data /root/gbr/yolov5/reef_f1_naive.yaml --weights yolov5m6.pt --name l6_3600_uflip_vm5_m_f2 --hyp /root/gbr/yolov5/hyp.heavy.2.yaml
-python3 train.py --img 6500 --batch 2 --epochs 7 --data /root/gbr/yolov5/reef_f1_naive.yaml --weights /root/kaggle/tensorflow-great-barrier-reef/models/pretrained_weights/best_f1.pt --freeze 10 --name f1_fineturning --hyp /root/gbr/yolov5/hyp.heavy.2.yaml
+python3 train.py --img 6500 --batch 2 --epochs 5 --data /root/gbr/yolov5/reef_f1_naive.yaml --weights /root/kaggle/tensorflow-great-barrier-reef/models/pretrained_weights/best_f1.pt --freeze 10 --name f2_fineturning --hyp /root/gbr/yolov5/hyp.heavy.2.yaml
