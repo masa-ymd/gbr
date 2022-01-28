6fc98d115131ab8f3b1e2990ee8e977ddc34900c
masa-ymd <masa-ymd@nifty.com>
2022-01-28 17:52:29 +0000

    x

--- yolov5/val.sh
+++ yolov5/val.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /root/kaggle/yolov5
-python3 val.py --imgsz 3600 --batch 2 --data /root/gbr/yolov5/reef_f1_naive.yaml --weights /root/kaggle/yolov5/runs//train/f1_fineturning8/weights/best.pt --conf-thres 0.01 --iou-thres 0.3 --save-txt --save-conf --exist-ok
+python3 val.py --imgsz 7200 --batch 1 --data /root/gbr/yolov5/reef_f1_naive.yaml --weights /root/kaggle/yolov5/runs//train/f1_fineturning8/weights/best.pt --conf-thres 0.01 --iou-thres 0.3 --save-txt --save-conf --exist-ok
