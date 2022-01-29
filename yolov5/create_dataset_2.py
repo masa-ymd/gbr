ad713c311efff1d3b60b0d817393126caf71fc2e
masa-ymd <masa.ymd.0703@gmail.com>
2022-01-30 02:00:17 +0900

    x

--- yolov5/create_dataset_2.py
+++ yolov5/create_dataset_2.py
@@ -1,3 +1,7 @@
+import os
+from shutil import copyfile
+from tqdm import tqdm
+tqdm.pandas()
 import pandas as pd
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_colwidth", None)
@@ -18,3 +22,15 @@ def add_new_path(row):
 df['new_path'] = df.apply(lambda row: add_new_path(row), axis=1)
 print("New image path for train/valid created")
 print(df.head(3))
+
+os.makedirs(f"{DATA_DIR}/images/train", exist_ok=True)
+os.makedirs(f"{DATA_DIR}/images/valid", exist_ok=True)
+os.makedirs(f"{DATA_DIR}/labels/train", exist_ok=True)
+os.makedirs(f"{DATA_DIR}/labels/valid", exist_ok=True)
+print(f"Directory structure yor Yolov5 created")
+
+#def copy_file(row):
+#    copyfile(row.image_path, row.new_path)
+    
+_ = df.progress_apply(lambda row: copyfile(row.image_path, row.new_path), axis=1)
+print("Sucessfully copy file for train and valid")
