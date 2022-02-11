9ee8694d73203a13c45a6e0c4b478c6bef0eb421
masa-ymd <masa-ymd@nifty.com>
2022-02-11 01:03:33 +0000

    x

--- yolov5/train.sh
+++ yolov5/train.sh
@@ -2,7 +2,8 @@
 
 cd /root/kaggle/yolov5
 #python3 train.py --img 2000 --batch 8 --epochs 15 --data /root/gbr/yolov5/reef_90_10_1982.yaml --weights yolov5s6.pt --name split_90_10_5s6_onlyano_bs8_ --hyp /root/gbr/yolov5/hyp.heavy.2.yaml
-python3 train.py --img 1400 --batch 8 --epochs 15 --data /root/gbr/yolov5/reef_90_10_1982.yaml --weights yolov5m6.pt --name split_90_10_5m6_onlyano_bs8_ --hyp /root/gbr/yolov5/hyp.heavy.2.yaml --save-period 1
+#python3 train.py --img 1400 --batch 8 --epochs 15 --data /root/gbr/yolov5/reef_90_10_1982.yaml --weights yolov5m6.pt --name split_90_10_5m6_onlyano_bs8_ --hyp /root/gbr/yolov5/hyp.heavy.2.yaml --save-period 1
+python3 train.py --img 1400 --batch 16 --epochs 10 --data /root/gbr/yolov5/reef_90_10_1982.yaml --weights /root/kaggle/yolov5/runs/train/split_90_10_5m6_onlyano_bs8_3/weights/epoch10.pt --name split_90_10_5m6_onlyano_bs8_ft_ --freeze 10 --hyp /root/gbr/yolov5/hyp.heavy.2.yaml --save-period 1
 #python3 train.py --img 3000 --batch 2 --epochs 15 --data /root/gbr/yolov5/reef_f1_naive.yaml --weights yolov5m6.pt --name l6_3600_uflip_vm5_m_f2 --hyp /root/gbr/yolov5/hyp.heavy.2.yaml
 #python3 train.py --img 4000 --batch 4 --epochs 20 --data /root/gbr/yolov5/reef_f1_naive.yaml --weights /root/kaggle/tensorflow-great-barrier-reef/models/pretrained_weights/best_f1.pt --freeze 10 --name f1_fineturning --hyp /root/gbr/yolov5/hyp.heavy.2.yaml
 #python3 train.py --img 3000 --batch 2 --epochs 15 --data /root/gbr/yolov5/reef_80_20_naive.yaml --weights yolov5m.pt --name split_80_20_5m --hyp /root/gbr/yolov5/hyp.heavy.2.yaml
